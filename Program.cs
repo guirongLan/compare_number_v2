cc4ae3ce0ec671277e9de08330c014f42e035edc
guirongLan <46593774+guirongLan@users.noreply.github.com>
2022-02-03 16:42:08 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5个数字
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double primoNumero, secondoNumero;
+            Console.WriteLine("Inserisci primo valore");
+            secondoNumero = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Inserisci secondo valore");
+            primoNumero = Convert.ToDouble(Console.ReadLine());
+            if (secondoNumero < primoNumero)
+            {
+                secondoNumero = primoNumero;
+            }
+            Console.WriteLine("Inserisci terzo valore");
+            primoNumero = Convert.ToDouble(Console.ReadLine());
+            if (secondoNumero < primoNumero)
+            {
+                secondoNumero = primoNumero;
+            }
+            Console.WriteLine("Inserisci quart" +
+                "o valore");
+            primoNumero = Convert.ToDouble(Console.ReadLine());
+            if (secondoNumero < primoNumero)
+            {
+                secondoNumero = primoNumero;
+            }
+            Console.WriteLine("Inserisci quindo valore");
+            primoNumero = Convert.ToDouble(Console.ReadLine());
+            if (secondoNumero < primoNumero)
+            {
+                secondoNumero = primoNumero;
+            }
+            Console.WriteLine("il numero piu grande :" + secondoNumero);
+            Console.ReadKey();
+        }
+    }
+}
